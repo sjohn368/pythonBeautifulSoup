9d5424d660f5ce2e8e52034e3c8f5eb870fb0ac3
Nehalk145 <89485450+Nehalk145@users.noreply.github.com>
2022-03-30 12:08:33 -0400

    Add files via upload

---
+++ ouline.py
@@ -0,0 +1,21 @@
+#import beautifulsoup and request here
+
+
+#function to get job list from url 'https://www.indeed.com/jobs?q={role}&l={location}'
+def getJobList():
+    url = 'https://www.indeed.com/jobs?q={role}&l={location}'
+    # Complete the missing part of this fuction here 
+
+
+
+#main function
+def main():
+    # Write a code here to get job location and role from user e.g. role = input()
+    print("Enter role you want to search")
+    role = input()
+    # Complete the missing part of this fuction here
+
+
+
+if __name__ == '__main__':
+    main()
