ea70763e1ab005410738f26569633193b5286d86
Nehalk145 <89485450+Nehalk145@users.noreply.github.com>
2022-03-30 13:05:01 -0400

    Update ouline.py

--- ouline.py
+++ ouline.py
@@ -4,11 +4,11 @@
 #function to get job list from url 'https://www.indeed.com/jobs?q={role}&l={location}'
 def getJobList():
     url = 'https://www.indeed.com/jobs?q={role}&l={location}'
-    # Complete the missing part of this fuction here 
+    # Complete the missing part of this function here 
 
 #save data in csv file
 def saveDataInCSV():
-    #Complete the missing part of this fuction here
+    #Complete the missing part of this function here
     print("Saving data to csv")
 
 #main function
@@ -16,7 +16,12 @@ def main():
     # Write a code here to get job location and role from user e.g. role = input()
     print("Enter role you want to search")
     role = input()
-    # Complete the missing part of this fuction here
+    # Complete the missing part of this function here
+    print("Enter location: ")
+    location = input()
+    print("Job role: ", role)
+    print("location: ", location)
+    getJobList(role,location)
 
 
 
